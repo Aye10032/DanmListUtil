73d50805c7cb85498b3d0f99450106ebb90652c2
Aye10032 <yeyu6_505@126.com>
2020-03-31 13:35:04 +0800

    update http

--- src/main/java/MainRun.java
+++ src/main/java/MainRun.java
@@ -1,40 +1,21 @@
 import com.aye10032.util.BiliUtil;
 import com.aye10032.util.FileUtil;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 
 
 public class MainRun {
 
     public static void main(String[] args) {
-        new BiliUtil().getVideoList("40077740");
-
-        /*try {
-            OkHttpClient client = new OkHttpClient().newBuilder()
-                    .build();
-            Request request = new Request.Builder()
-                    .url("https://api.bilibili.com/x/v1/dm/list.so?oid=171147896")
-                    .method("GET", null)
-                    .addHeader("Host", "api.bilibili.com")
-                    .addHeader("Connection", "keep-alive")
-                    .addHeader("Cache-Control", "max-age=0")
-                    .addHeader("Upgrade-Insecure-Requests", "1")
-                    .addHeader("Accept", "text/xml")
-                    .addHeader("Accept-Encoding","gzip, deflate, br")
-                    .addHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36")
-                    .build();
-            Response response = client.newCall(request).execute();
-
-            System.out.println(response.body().string());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
+        String xmlStr = new BiliUtil().getDanmu("171147896");
     }
 
 }
--- src/main/java/com/aye10032/util/BiliUtil.java
+++ src/main/java/com/aye10032/util/BiliUtil.java
@@ -4,9 +4,15 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,33 +22,31 @@ public class BiliUtil {
 
     static String api_up = "https://api.bilibili.com/x/web-interface/card?mid=";
     static String api_video = "http://space.bilibili.com/ajax/member/getSubmitVideos?mid=";
+    static String api_danmu = "https://api.bilibili.com/x/v1/dm/list.so?oid=";
 
-    OkHttpClient client = null;
+    CloseableHttpClient httpclient;
 
-    public BiliUtil(){
-
+    public BiliUtil() {
+        httpclient = HttpClients.createDefault();
     }
 
-    public List<String> getVideoList(String UUID, int pageSize){
+    public List<String> getVideoList(String UUID, int pageSize) {
         List<String> videolist = new ArrayList<String>();
 
         StringBuilder urlBuilder = new StringBuilder();
         urlBuilder.append(api_video).append(UUID).append("&pagesize=").append(pageSize);
 
+        CloseableHttpResponse response = null;
         try {
-            client = new OkHttpClient().newBuilder()
-                    .build();
-            Request request = new Request.Builder()
-                    .url(new String(urlBuilder))
-                    .method("GET", null)
-                    .build();
-            Response response = client.newCall(request).execute();
+            HttpGet httpget = new HttpGet(new String(urlBuilder));
 
-            System.out.println(response.body().string());
+            response = httpclient.execute(httpget);
 
             String body = null;
-            if (response.body() != null){
-                body = new String(response.body().bytes());
+            if (HttpStatus.SC_OK == response.getStatusLine().getStatusCode()) {//判断状态码
+                HttpEntity httpEntity = response.getEntity(); //获取返回body
+                body = EntityUtils.toString(httpEntity, "UTF-8");// 转成string
+                System.out.println("result:" + body);
             }
 
             JsonParser jsonParser = new JsonParser();
@@ -53,25 +57,76 @@ public class BiliUtil {
 
                 JsonArray vlist = jsonObject.getAsJsonObject("data").getAsJsonArray("vlist");
 
-                for (JsonElement listElement:vlist){
-                    if (listElement.isJsonObject()){
+                for (JsonElement listElement : vlist) {
+                    if (listElement.isJsonObject()) {
                         JsonObject listObject = listElement.getAsJsonObject();
 
                         videolist.add(listObject.get("aid").getAsString());
                     }
                 }
             }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (response != null) {
+                    response.close(); //释放连接
+                }
+            } catch (Exception e) {
+            }
+            try {
+                if (httpclient != null) {
+                    httpclient.close();//关闭客户端
+                }
+            } catch (Exception e) {
+            }
         }
 
         return videolist;
     }
 
-    public List<String> getVideoList(String UUID){
+    public List<String> getVideoList(String UUID) {
         List<String> videolist = new ArrayList<String>();
 
-        return getVideoList(UUID,10);
+        return getVideoList(UUID, 10);
+    }
+
+    public String getDanmu(String aid){
+        StringBuilder urlBuilder = new StringBuilder();
+        urlBuilder.append(api_danmu).append(aid);
+
+        HttpGet httpget = new HttpGet(new String(urlBuilder));
+
+        CloseableHttpResponse response = null;
+        String body = null;
+        try {
+            response = httpclient.execute(httpget);
+
+
+            if(HttpStatus.SC_OK == response.getStatusLine().getStatusCode()){
+                HttpEntity httpEntity = response.getEntity();
+                body = EntityUtils.toString(httpEntity,"UTF-8");
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try{
+                if(response != null){
+                    response.close(); //释放连接
+                }
+            }catch(Exception e){}
+            try{
+                if(httpclient != null){
+                    httpclient.close();//关闭客户端
+                }
+            }catch(Exception e){}
+        }
+
+        return body;
     }
 
 }
